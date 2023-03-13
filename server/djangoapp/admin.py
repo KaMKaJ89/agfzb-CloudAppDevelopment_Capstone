96d7eda3089fb59da285001f9bf1b5da7b3dc274
KaMKaJ89 <yokamkaj89@gmail.com>
2023-03-12 20:54:18 -0400

    Adding temporary changes to Github

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,29 @@
 from django.contrib import admin
-# from .models import related models
+from .models import CarMake, CarModel
 
 
 # Register your models here.
 
+
+
 # CarModelInline class
+class CarModelInLine(admin.StackedInline):
+    model = CarModel
+    extra = 5
 
 # CarModelAdmin class
+class CarModelAdmin(admin.ModelAdmin):
+    list_display = ('model_make','model_name', 'model_year')
+    list_filter = ['model_make']
+    search_fields = ['name', 'description']
 
 # CarMakeAdmin class with CarModelInline
+class CarMakeAdmin(admin.ModelAdmin):
+    inlines = [CarModelInLine]
+    list_display = ('model_make','model_name', 'model_year')
+    list_filter = ['model_make']
+    search_fields = ['name', 'description']
 
 # Register models here
+admin.site.register(CarMake)
+admin.site.register(CarModel)
--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -9,6 +9,14 @@ from django.utils.timezone import now
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
+# User model
+class CarMake(models.Model):
+    make_name = models.CharField(null=False, max_length=30, default='Audi')
+    make_description = models.CharField(null=False, max_length=30, default='description')
+    
+    # Create a toString method for object string representation
+    def __str__(self):
+        return self.make_name + " - " + self.make_description
 
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
@@ -19,6 +27,24 @@ from django.utils.timezone import now
 # - Year (DateField)
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
+class CarModel(models.Model):
+    SEDAN = 'sedan'
+    SUV = 'suv'
+    WAGON = 'vagon'
+    CAR_TYPES = [
+        (SEDAN, 'Sedan'),
+        (SUV, 'SUV'),
+        (WAGON, 'WAGON')
+    ]
+    model_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    model_dealer = models.IntegerField(null=False)
+    model_name = models.CharField(null=False, max_length=30, default='Audi')
+    model_type = models.CharField(max_length=30, choices=CAR_TYPES, default=SEDAN)
+    model_year = models.DateField(null=False, max_length=30)
+    
+    # Create a toString method for object string representation
+    def __str__(self):
+        return self.model_name + " " + " (" + self.model_type + ")"
 
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
